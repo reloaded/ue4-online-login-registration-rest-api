fc580972d2eb9b97fc161e633fb7b6fac531723b
Jason Harris <jason@cunexus.com>
2017-03-01 14:43:46 -0800

    Add generated phalcon models

---
+++ src/app/models/PlayerAccountRecovery.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace Reloaded\UnrealEngine4\Models;
+
+class PlayerAccountRecovery extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var string
+     * @Primary
+     * @Column(type="string", length=16, nullable=false)
+     */
+    protected $PlayerId;
+
+    /**
+     *
+     * @var string
+     * @Column(type="string", length=10, nullable=false)
+     */
+    protected $Code;
+
+    /**
+     *
+     * @var string
+     * @Column(type="string", nullable=false)
+     */
+    protected $Expiration;
+
+    /**
+     *
+     * @var string
+     * @Column(type="string", nullable=false)
+     */
+    protected $GeneratedOn;
+
+    /**
+     *
+     * @var string
+     * @Column(type="string", nullable=false)
+     */
+    protected $Type;
+
+    /**
+     * Method to set the value of field PlayerId
+     *
+     * @param string $PlayerId
+     * @return $this
+     */
+    public function setPlayerId($PlayerId)
+    {
+        $this->PlayerId = $PlayerId;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field Code
+     *
+     * @param string $Code
+     * @return $this
+     */
+    public function setCode($Code)
+    {
+        $this->Code = $Code;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field Expiration
+     *
+     * @param string $Expiration
+     * @return $this
+     */
+    public function setExpiration($Expiration)
+    {
+        $this->Expiration = $Expiration;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field GeneratedOn
+     *
+     * @param string $GeneratedOn
+     * @return $this
+     */
+    public function setGeneratedOn($GeneratedOn)
+    {
+        $this->GeneratedOn = $GeneratedOn;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field Type
+     *
+     * @param string $Type
+     * @return $this
+     */
+    public function setType($Type)
+    {
+        $this->Type = $Type;
+
+        return $this;
+    }
+
+    /**
+     * Returns the value of field PlayerId
+     *
+     * @return string
+     */
+    public function getPlayerId()
+    {
+        return $this->PlayerId;
+    }
+
+    /**
+     * Returns the value of field Code
+     *
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->Code;
+    }
+
+    /**
+     * Returns the value of field Expiration
+     *
+     * @return string
+     */
+    public function getExpiration()
+    {
+        return $this->Expiration;
+    }
+
+    /**
+     * Returns the value of field GeneratedOn
+     *
+     * @return string
+     */
+    public function getGeneratedOn()
+    {
+        return $this->GeneratedOn;
+    }
+
+    /**
+     * Returns the value of field Type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->Type;
+    }
+
+    /**
+     * Initialize method for model.
+     */
+    public function initialize()
+    {
+        $this->setSchema("ue4");
+        $this->belongsTo('PlayerId', 'Reloaded\UnrealEngine4\Models\\Players', 'Id', ['alias' => 'Players']);
+    }
+
+    /**
+     * Allows to query a set of records that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return PlayerAccountRecovery[]|PlayerAccountRecovery
+     */
+    public static function find($parameters = null)
+    {
+        return parent::find($parameters);
+    }
+
+    /**
+     * Allows to query the first record that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return PlayerAccountRecovery
+     */
+    public static function findFirst($parameters = null)
+    {
+        return parent::findFirst($parameters);
+    }
+
+    /**
+     * Returns table name mapped in the model.
+     *
+     * @return string
+     */
+    public function getSource()
+    {
+        return 'player_account_recovery';
+    }
+
+}
---
+++ src/app/models/PlayerSessions.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace Reloaded\UnrealEngine4\Models;
+
+class PlayerSessions extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var string
+     * @Primary
+     * @Column(type="string", length=16, nullable=false)
+     */
+    protected $PlayerId;
+
+    /**
+     *
+     * @var string
+     * @Column(type="string", length=16, nullable=false)
+     */
+    protected $SessionId;
+
+    /**
+     *
+     * @var string
+     * @Column(type="string", nullable=false)
+     */
+    protected $Expiration;
+
+    /**
+     *
+     * @var string
+     * @Column(type="string", length=16, nullable=false)
+     */
+    protected $RemoteIp;
+
+    /**
+     *
+     * @var string
+     * @Column(type="string", nullable=false)
+     */
+    protected $Created;
+
+    /**
+     * Method to set the value of field PlayerId
+     *
+     * @param string $PlayerId
+     * @return $this
+     */
+    public function setPlayerId($PlayerId)
+    {
+        $this->PlayerId = $PlayerId;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field SessionId
+     *
+     * @param string $SessionId
+     * @return $this
+     */
+    public function setSessionId($SessionId)
+    {
+        $this->SessionId = $SessionId;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field Expiration
+     *
+     * @param string $Expiration
+     * @return $this
+     */
+    public function setExpiration($Expiration)
+    {
+        $this->Expiration = $Expiration;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field RemoteIp
+     *
+     * @param string $RemoteIp
+     * @return $this
+     */
+    public function setRemoteIp($RemoteIp)
+    {
+        $this->RemoteIp = $RemoteIp;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field Created
+     *
+     * @param string $Created
+     * @return $this
+     */
+    public function setCreated($Created)
+    {
+        $this->Created = $Created;
+
+        return $this;
+    }
+
+    /**
+     * Returns the value of field PlayerId
+     *
+     * @return string
+     */
+    public function getPlayerId()
+    {
+        return $this->PlayerId;
+    }
+
+    /**
+     * Returns the value of field SessionId
+     *
+     * @return string
+     */
+    public function getSessionId()
+    {
+        return $this->SessionId;
+    }
+
+    /**
+     * Returns the value of field Expiration
+     *
+     * @return string
+     */
+    public function getExpiration()
+    {
+        return $this->Expiration;
+    }
+
+    /**
+     * Returns the value of field RemoteIp
+     *
+     * @return string
+     */
+    public function getRemoteIp()
+    {
+        return $this->RemoteIp;
+    }
+
+    /**
+     * Returns the value of field Created
+     *
+     * @return string
+     */
+    public function getCreated()
+    {
+        return $this->Created;
+    }
+
+    /**
+     * Initialize method for model.
+     */
+    public function initialize()
+    {
+        $this->setSchema("ue4");
+        $this->belongsTo('PlayerId', 'Reloaded\UnrealEngine4\Models\\Players', 'Id', ['alias' => 'Players']);
+    }
+
+    /**
+     * Allows to query a set of records that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return PlayerSessions[]|PlayerSessions
+     */
+    public static function find($parameters = null)
+    {
+        return parent::find($parameters);
+    }
+
+    /**
+     * Allows to query the first record that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return PlayerSessions
+     */
+    public static function findFirst($parameters = null)
+    {
+        return parent::findFirst($parameters);
+    }
+
+    /**
+     * Returns table name mapped in the model.
+     *
+     * @return string
+     */
+    public function getSource()
+    {
+        return 'player_sessions';
+    }
+
+}
