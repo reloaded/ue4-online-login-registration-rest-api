1082bdf9e4e59f5814f39bf71408ae7b478fad26
Jason Harris <jason@cunexus.com>
2017-03-03 18:53:17 -0800

    Add session controller with register action

---
+++ src/app/controllers/SessionController.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * @author Jason Harris <1337reloaded@gmail.com>
+ * @date 2/28/2017 4:15 PM
+ */
+
+namespace Reloaded\UnrealEngine4\Web\Controllers;
+
+use App\Library\Net\HttpStatusCode;
+use App\Library\Net\Responses\DataObjectResponse;
+use App\Library\Net\Responses\IDataObject;
+use App\Library\Net\Responses\ValidationFaultResponse;
+use App\Library\Net\Responses\ValidationFieldError;
+use App\Library\Requests\RegistrationRequest;
+use App\Library\Requests\RegistrationRequestValidator;
+use Ramsey\Uuid\Uuid;
+use Reloaded\UnrealEngine4\Models\Players;
+
+class SessionController extends ControllerBase
+{
+
+    public function registerAction()
+    {
+        try
+        {
+            $this->db->begin();
+
+            /** @var $registrationRequest RegistrationRequest */
+            $registrationRequest = $this->mapper->map($this->request->getJsonRawBody(), new RegistrationRequest());
+
+            $requestValidation = new RegistrationRequestValidator();
+            $requestErrors = $requestValidation->validate($registrationRequest);
+
+            if(count($requestErrors))
+            {
+                $validationFault = new ValidationFaultResponse(HttpStatusCode::BadRequest);
+                $validationFault->addPhalconValidationGroup($requestValidation->getMessages());
+
+                return $this->response
+                    ->setStatusCode(HttpStatusCode::BadRequest)
+                    ->setJsonContent($validationFault);
+            }
+
+            $guid = Uuid::uuid4();
+
+            $player = new Players([
+                "Id" => $guid->toString(),
+                "FirstName" => $registrationRequest->FirstName,
+                "LastName" => $registrationRequest->LastName,
+                "Email" => $registrationRequest->Email,
+                "InGameName" => $registrationRequest->InGameName,
+                "Password" => password_hash($registrationRequest->Password, PASSWORD_DEFAULT)
+            ]);
+
+            if(!$player->save())
+            {
+                $this->db->rollback();
+
+                $validationFault = new ValidationFaultResponse(HttpStatusCode::BadRequest);
+                $validationFault->addPhalconModelMessages($player->getMessages());
+
+                return $this->response
+                    ->setStatusCode(HttpStatusCode::BadRequest)
+                    ->setJsonContent($validationFault);
+            }
+
+            $this->db->commit();
+
+            $responseData = (object) [
+                "Id" => $player->getId(),
+                "FirstName" => $registrationRequest->FirstName,
+                "LastName" => $registrationRequest->LastName,
+                "Email" => $registrationRequest->Email,
+                "InGameName" => $registrationRequest->InGameName
+            ];
+
+            return $this->response
+                ->setStatusCode(HttpStatusCode::Created)
+                ->setJsonContent(new DataObjectResponse($responseData, HttpStatusCode::Created));
+        }
+        catch(\Exception $ex)
+        {
+            $this->db->rollback();
+        }
+    }
+
+    public function loginAction()
+    {
+
+    }
+
+    public function logoutAction()
+    {
+
+    }
+
+}
+
