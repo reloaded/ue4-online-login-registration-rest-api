c27748126b1abf0d5b6a98c460846ee0698a5785
Jason Harris <jason@cunexus.com>
2017-03-04 16:37:18 -0800

    Add Players model validation class, update Players model so it uses it's Validation class.

--- src/app/models/Players.php
+++ src/app/models/Players.php
@@ -2,11 +2,7 @@
 
 namespace Reloaded\UnrealEngine4\Models;
 
-use Phalcon\Validation;
-use Phalcon\Validation\Validator\Alnum;
-use Phalcon\Validation\Validator\Alpha;
-use Phalcon\Validation\Validator\Email;
-use Phalcon\Validation\Validator\StringLength;
+use App\Models\Validation\Players as PlayersValidation;
 use Ramsey\Uuid\Uuid;
 
 class Players extends AbstractPlayers
@@ -31,50 +27,7 @@ class Players extends AbstractPlayers
 
     public function validation()
     {
-        $validation = new Validation();
-
-        $validation->setFilters('InGameName', 'trim');
-
-        $validation->add('InGameName', new StringLength([
-            'max' => 25,
-            'min' => 4
-        ]));
-
-        $validation->add('InGameName', new Alnum([
-            'message' => 'In-Game Name must contain only alphanumeric characters.'
-        ]));
-
-        $validation->add('Email', new Email([
-            'message' => 'Please enter a valid email.'
-        ]));
-
-        $validation->add('Email', new StringLength([
-            'message' => 'We only accept emails up to 255 characters long.',
-            'max' => 255,
-            'min' => 6
-        ]));
-
-        $validation->setFilters(['FirstName', 'LastName'], 'trim');
-
-        $validation->add(['FirstName', 'LastName'], new StringLength([
-            'max' => 50,
-            'min' => 2,
-            'messageMaximum' => [
-                'FirstName' => 'First name can only be up to 50 characters long.',
-                'LastName' => 'Last name can only be up to 50 characters long.'
-            ],
-            'messageMinimum' => [
-                'FirstName' => 'First name must be at least 2 characters.',
-                'LastName' => 'Last name must be at least 2 characters.'
-            ]
-        ]));
-
-        $validation->add(['FirstName', 'LastName'], new Alpha([
-            'message' => [
-                'FirstName' => 'First name can only contain alphabetic characters.',
-                'LastName' => 'Last name can only contain alphabetic characters.'
-            ]
-        ]));
+        $validation = new PlayersValidation();
 
         return $this->validate($validation);
     }
