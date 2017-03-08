2be37fc18b22a1b6ef792ba0316cd89cdde85863
Jason Harris <jason@cunexus.com>
2017-03-07 23:31:34 -0800

    Add Code validation component for PlayerAccountRecovery.

---
+++ src/app/validation/PlayerAccountRecovery/Code.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @author Jason Harris <1337reloaded@gmail.com>
+ * @date 3/7/2017 11:28 PM
+ */
+
+namespace App\Validation\PlayerAccountRecovery;
+
+
+use Phalcon\Validation;
+use Phalcon\Validation\Validator\StringLength;
+
+/**
+ * Validates a random unique recovery code associated to a PlayerAccountRecovery model.
+ *
+ * @package App\Validation\PlayerAccountRecovery
+ */
+class Code extends Validation
+{
+    public function initialize()
+    {
+        $this->add(
+            'Code',
+            new StringLength([
+                'min' => 10,
+                'max' => 10,
+                'messageMinimum' => 'Recovery code must be 10 characters.',
+                'messageMaximum' => 'Recovery code must be 10 characters.'
+            ])
+        );
+    }
+}
